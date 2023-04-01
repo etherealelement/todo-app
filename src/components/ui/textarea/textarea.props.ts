b5d83e68a32861f183c82efe0d4a8b14d795f959
ethereal element <vitalik.safonov@list.ru>
2023-04-01 20:23:01 +0300

    create textarea ui with react-hook-form

--- src/components/ui/textarea/textarea.props.ts
+++ src/components/ui/textarea/textarea.props.ts
@@ -2,7 +2,6 @@ import { InputHTMLAttributes } from "react";
 
 export interface InputTextarea extends InputHTMLAttributes<HTMLAllCollection> {
 	name: string;
-	value: string;
 	className: string;
 	placeholder?: string;
 }
--- src/components/ui/textarea/textarea.tsx
+++ src/components/ui/textarea/textarea.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
 import { InputTextarea } from './textarea.props';
 import styles from "./textarea.module.css";
-
+import { SubmitHandler, useForm} from "react-hook-form";
 
 export const Textarea: FC<InputTextarea> = ({name, placeholder,value, className, ...props}) => {
-	return <div>
-		<textarea placeholder={placeholder} className={styles.textarea}></textarea>
-	</div>;
+	const {register , handleSubmit} = useForm<InputTextarea>();
+
+	const onSubmit: SubmitHandler<InputTextarea> = (data) => {
+		alert(`Данные получены! от ${data.name}`)
+	}
+
+	return <>
+		<form onSubmit={handleSubmit(onSubmit)}>
+
+	<textarea {...register("name")} className={styles.textarea}>
+</textarea>
+
+		</form>
+	</>
 };
